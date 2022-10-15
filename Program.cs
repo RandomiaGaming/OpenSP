46a8438368a852d55f54c026eb81635495d1f655
RandomiaGaming <RandomiaGaming@ACER-NITRO-5>
2022-10-14 23:00:18 -0700

    Woah its a sound board

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System;
+using System.Media;
+using NAudio;
+using NAudio.Wave;
+using System.Threading;
+
+namespace YAGMCBSoundPanel
+{
+    public static class Program
+    {
+        public static string SoundQueDirectory = "C:\\Users\\RandomiaGaming\\Desktop\\YAGMCB Soundtrack\\YAGMCB Performance Soundtrack";
+        public static SoundPlayer[] SoundQues;
+        public static int PlayingSoundIndex = 0;
+        public static void Main()
+        {
+            string[] soundQueFilePaths = Directory.GetFiles(SoundQueDirectory);
+            SoundQues = new SoundPlayer[soundQueFilePaths.Length];
+            for (int i = 0; i < soundQueFilePaths.Length; i++)
+            {
+                SoundQues[i] = new SoundPlayer(soundQueFilePaths[i]);
+            }
+            PlaySound(0);
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                if (consoleKeyInfo.KeyChar is 'd')
+                {
+                    SkipFowards();
+                }
+                else if (consoleKeyInfo.KeyChar is 'a')
+                {
+                    SkipBackwards();
+                }
+                else if (consoleKeyInfo.KeyChar is 's')
+                {
+                    Stop();
+                }
+                else if (consoleKeyInfo.KeyChar is 'r')
+                {
+                    Replay();
+                }
+            }
+        }
+        public static void Replay()
+        {
+            PlaySound(PlayingSoundIndex);
+        }
+        public static void SkipFowards()
+        {
+            if (PlayingSoundIndex == SoundQues.Length - 1)
+            {
+                PlaySound(PlayingSoundIndex);
+            }
+            else
+            {
+                PlaySound(PlayingSoundIndex + 1);
+            }
+        }
+        public static void SkipBackwards()
+        {
+            if (PlayingSoundIndex is 0)
+            {
+                Replay();
+            }
+            else
+            {
+                PlaySound(PlayingSoundIndex - 1);
+            }
+        }
+        public static void PlaySound(int index)
+        {
+            if (index < 0 || index >= SoundQues.Length)
+            {
+                throw new Exception("index must be greater than or equal to 0 and less than sound count.");
+            }
+            if (PlayingSoundIndex != -1)
+            {
+                SoundQues[PlayingSoundIndex].Playing = false;
+                SoundQues[PlayingSoundIndex].StreamPosition = 0;
+            }
+            PlayingSoundIndex = index;
+            SoundQues[PlayingSoundIndex].audioFileReader.Position = 0;
+            SoundQues[PlayingSoundIndex].waveOutputDevice.Play();
+        }
+        public static void Stop()
+        {
+            SoundQues[PlayingSoundIndex].Playing = false;
+            SoundQues[PlayingSoundIndex].StreamPosition = 0;
+        }
+    }
+}
---
+++ SoundPlayer.cs
@@ -0,0 +1,96 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGMCBSoundPanel
+{
+    public sealed class SoundPlayer
+    {
+        public bool Playing
+        {
+            get
+            {
+                return waveOutputDevice.PlaybackState == PlaybackState.Playing;
+            }
+            set
+            {
+                if (value)
+                {
+                    if (!(waveOutputDevice.PlaybackState is PlaybackState.Playing))
+                    {
+                        waveOutputDevice.Play();
+                    }
+                }
+                else
+                {
+                    if (waveOutputDevice.PlaybackState is PlaybackState.Playing)
+                    {
+                        waveOutputDevice.Pause();
+                    }
+                }
+            }
+        }
+        public TimeSpan Position
+        {
+            get
+            {
+                return audioFileReader.CurrentTime;
+            }
+            set
+            {
+                audioFileReader.CurrentTime = value;
+            }
+        }
+        public long StreamPosition
+        {
+            get
+            {
+                return audioFileReader.Position;
+            }
+            set
+            {
+                audioFileReader.Position = value;
+            }
+        }
+        public TimeSpan Duration
+        {
+            get
+            {
+                return audioFileReader.TotalTime;
+            }
+        }
+        public long StreamLength
+        {
+            get
+            {
+                return audioFileReader.Length;
+            }
+        }
+        public float Volume
+        {
+            get
+            {
+                return waveOutputDevice.Volume;
+            }
+            set
+            {
+                waveOutputDevice.Volume = value;
+            }
+        }
+
+        public WaveOut waveOutputDevice;
+        public AudioFileReader audioFileReader;
+        public SoundPlayer(string filePath)
+        {
+            waveOutputDevice = new WaveOut();
+            audioFileReader = new AudioFileReader(filePath);
+            waveOutputDevice.Init(audioFileReader);
+            waveOutputDevice.Play();
+            waveOutputDevice.Pause();
+            audioFileReader.Position = 0;
+        }
+    }
+}
